133e1c54e8c8128cdc0377a639d3006cbcd92eb6
ph9ii <moattia86@gmail.com>
2019-04-26 03:39:30 +0200

    knex bookshelf

---
+++ bookshelf.js
@@ -0,0 +1,4 @@
+const knex = require('knex');
+const bookshelf = require('bookshelf');
+const knexConfig = require('./knexfile');
+module.exports = bookshelf(knex(knexConfig.development));
---
+++ knexfile.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  },
+
+  staging: {
+    client: 'mysql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  }
+
+};
