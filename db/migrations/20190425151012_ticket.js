759ccb9d2f8e59a01f7e5eedc22e8f3e26372a02
ph9ii <moattia86@gmail.com>
2019-04-27 04:34:11 +0200

    update ticket schema

--- db/migrations/20190425151012_ticket.js
+++ db/migrations/20190425151012_ticket.js
@@ -2,6 +2,17 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('ticket', function(table) {
     table.increments();
+    table.string('full_name', 255).notNullable();
+    table.string('email', 100).notNullable();
+    table.string('license_number', 50).notNullable();
+    table.string('color', 50).notNullable();
+    table.string('type', 50).notNullable();
+    table.string('description', 1000).notNullable();
+    table.dateTime('datetime').notNullable();
+    table.string('status', 50).notNullable().defaultTo('open');
+    table.string('day_phone', 100);
+    table.string('eve_phone', 100);
+    table.string('mob_phone', 100);
     table.integer("officer_id")
       .notNullable()
       .references("id")
@@ -9,12 +20,6 @@ exports.up = function(knex, Promise) {
       .unsigned()
       .onDelete("CASCADE")
       .index();
-    table.string('name', 50).notNullable();
-    table.string('email', 100).notNullable();
-    table.string('status', 50).notNullable().defaultTo('open');
-    table.string('day_phone', 100);
-    table.string('eve_phone', 100);
-    table.string('mob_phone', 100);
 
     table.timestamps(true, true);
   });
