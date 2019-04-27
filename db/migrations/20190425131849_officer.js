f88d7209fbee676a08bac0f22d9832f329f5e31a
ph9ii <moattia86@gmail.com>
2019-04-27 02:10:55 +0200

    officer table schema

---
+++ db/migrations/20190425131849_officer.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('officer', function(table) {
+    table.increments();
+    table.string('name', 50).notNullable();
+    table.string('email', 255).notNullable().unique();
+    table.string('password', 1024).notNullable();
+    table.boolean('avail').defaultTo(true);
+    table.boolean('admin').defaultTo(false);
+
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('officer');
+};
