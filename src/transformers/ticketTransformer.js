7c4fea6134f16db38cc165ee14a48cd4322e3fc1
ph9ii <moattia86@gmail.com>
2019-04-27 15:09:47 +0200

    Ticket Transformer HATEOAS

---
+++ src/transformers/ticketTransformer.js
@@ -0,0 +1,59 @@
+// tsc-check
+
+/**
+ * HATEOAS LINKS
+ *
+ * @params req
+ * @params hyperTicker collection
+ *
+ * @return array
+ */
+function getHyperLinks(req, hyperTicket) {
+  const baseUrl = req.protocol + '://' + req.headers.host;
+
+  const links = [
+    {
+      'rel': `self`,
+      'href': `${baseUrl}/api/tickets/${hyperTicket.id}`
+    },
+  ];
+
+  return links;
+}
+
+/**
+ * Defrag collection, get hyperLinks
+ *
+ * @params req
+ * @params collection
+ *
+ * @return collection
+ */
+module.exports.hyperMediaAll = function hyperMedia(req, collection) {
+  const hyperTickets= [];
+
+  collection.forEach(function (ticket, index, array) {
+    const hyperTicket = ticket;
+
+    hyperTicket.links = getHyperLinks(req, hyperTicket);
+
+    hyperTickets.push(hyperTicket);
+  });
+
+  return hyperTickets;
+}
+
+/**
+ * Defrag object, get hyperLinks
+ *
+ * @params req
+ * @params object
+ *
+ * @return object
+ */
+module.exports.hyperMediaOne = function hyperMedia(req, object) {
+  const hyperTicket = object.toJSON();
+  hyperTicket.links = getHyperLinks(req, hyperTicket);
+
+  return hyperTicket;
+}
