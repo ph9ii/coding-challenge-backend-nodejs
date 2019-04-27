c4abe2c83d0fb9397bc0c40aa1c72e3f54af6585
ph9ii <moattia86@gmail.com>
2019-04-27 15:21:56 +0200

    Audit schema

---
+++ db/migrations/20190425152020_audit.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('audit', function(table) {
+    table.increments();
+    table.integer("ticket_id")
+      .notNullable()
+    	.references("id")
+    	.inTable("ticket")
+    	.unsigned()
+      .onDelete("CASCADE")
+    	.index();
+    table.text("message").notNullable();
+    table.integer("code").notNullable();
+    
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('audit');
+};
