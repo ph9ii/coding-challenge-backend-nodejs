c1d841a5cb971357577f5b51f92adb4e31070ecf
ph9ii <moattia86@gmail.com>
2019-04-29 01:37:04 +0200

    add test knex config

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,35 @@
 // @ts-check
 
 require('dotenv').config();
+const config = require('config');
 
 module.exports = {
+  test: {
+    client: 'sqlite3',
+    connection: { 
+      // filename: "./mydb.sqlite" 
+      filename: ':memory:'
+    },
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  },
 
   development: {
     client: 'mysql',
     connection: {
-      database: process.env.MYSQL_DB_NAME,
-      user:     process.env.MYSQL_DB_USER,
-      password: process.env.MYSQL_DB_PASSWORD
+      database: config.get('MYSQL_DB_NAME'),
+      user:     config.get('MYSQL_DB_USER'),
+      password: config.get('MYSQL_DB_PASSWORD')
     },
     pool: {
       min: 2,
