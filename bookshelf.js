2a8d6489085e909f6ca7f1026e750249c995c5e5
ph9ii <moattia86@gmail.com>
2019-05-06 03:51:50 +0200

    fixing test env issue

--- bookshelf.js
+++ bookshelf.js
@@ -1,6 +1,11 @@
-// @ts-check
+const currentEnv = process.env.NODE_ENV;
 
 const knex = require('knex');
 const bookshelf = require('bookshelf');
 const knexConfig = require('./knexfile');
-module.exports = bookshelf(knex(knexConfig.development));
+if (currentEnv === 'test') {
+    module.exports = bookshelf(knex(knexConfig.test));
+} else {
+    module.exports = currentEnv === 'development' 
+    ? bookshelf(knex(knexConfig.development)) : bookshelf(knex(knexConfig.production));
+}
